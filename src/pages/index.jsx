1d28d07cadc670a3ebd77f95af82807ed3c7034a
Arunangshu Biswas <arunangshubsws@gmail.com>
2020-09-05 20:20:56 +0530

    Converted index.js to index.jsx

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Hero from "../components/Hero"
+import About from "../components/AboutMini"
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+    </>
+  )
+}
