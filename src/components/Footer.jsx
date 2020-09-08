d2ccf3aabddbebf6a44953cdfc35d7c3f1f91db0
Arunangshu Biswas <arunangshubsws@gmail.com>
2020-09-08 23:48:05 +0530

    Added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer id="footer" className="footer">
+      <div className="content has-text-centered">
+        <p>© Arunangshu Biswas, 2020</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,6 +6,7 @@ import Projects from "../components/Projects"
 import Skills from "../components/Skills"
 import Education from "../components/Education"
 import Contact from "../components/Contact"
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Skills />
       <Education />
       <Contact />
+      <Footer />
     </>
   )
 }
