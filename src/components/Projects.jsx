71d834892bfb6b5e750d3ee7384b06012b4d8b7a
Arunangshu Biswas <arunangshubsws@gmail.com>
2020-09-10 02:51:16 +0530

    feat: add preview and view code buttons to project items

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import { FaEye, FaCode } from "react-icons/fa"
 
 const Projects = () => {
   return (
     <section id="projects" className="has-background-white-ter pt-6 pb-6">
       <div className="container">
-        <h2 className="is-size-1 has-text-weight-bold has-text-centered mb-6">My Projects</h2>
+        <h2 className="is-size-1 has-text-weight-bold has-text-centered mb-6">
+          My Projects
+        </h2>
 
         <div className="columns is-multiline">
           <div className="column is-one-quarter">
@@ -20,6 +23,15 @@ const Projects = () => {
               <div className="card-content">
                 <p className="title">PROJECT 1</p>
                 <p className="subtitle">Lorem Ipsum Dolor Sit Amet</p>
+              </div>
+
+              <div className="card-footer">
+                <a href="#" className="card-footer-item">
+                  <FaEye className="is-size-4" />
+                </a>
+                <a href="#" className="card-footer-item">
+                  <FaCode className="is-size-4" />
+                </a>
               </div>
             </div>
           </div>
@@ -38,6 +50,15 @@ const Projects = () => {
                 <p className="title">PROJECT 2</p>
                 <p className="subtitle">Lorem Ipsum Dolor Sit Amet</p>
               </div>
+
+              <div className="card-footer">
+                <a href="#" className="card-footer-item">
+                  <FaEye className="is-size-4" />
+                </a>
+                <a href="#" className="card-footer-item">
+                  <FaCode className="is-size-4" />
+                </a>
+              </div>
             </div>
           </div>
 
@@ -54,6 +75,15 @@ const Projects = () => {
               <div className="card-content">
                 <p className="title">PROJECT 3</p>
                 <p className="subtitle">Lorem Ipsum Dolor Sit Amet</p>
+              </div>
+
+              <div className="card-footer">
+                <a href="#" className="card-footer-item">
+                  <FaEye className="is-size-4" />
+                </a>
+                <a href="#" className="card-footer-item">
+                  <FaCode className="is-size-4" />
+                </a>
               </div>
             </div>
           </div>
@@ -72,73 +102,14 @@ const Projects = () => {
                 <p className="title">PROJECT 4</p>
                 <p className="subtitle">Lorem Ipsum Dolor Sit Amet</p>
               </div>
-            </div>
-          </div>
 
-          <div className="column is-one-quarter">
-            <div className="card">
-              <div className="card-image">
-                <figure className="image is-4by3">
-                  <img
-                    src="https://bulma.io/images/placeholders/1280x960.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div className="card-content">
-                <p className="title">PROJECT 5</p>
-                <p className="subtitle">Lorem Ipsum Dolor Sit Amet</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="column is-one-quarter">
-            <div className="card">
-              <div className="card-image">
-                <figure className="image is-4by3">
-                  <img
-                    src="https://bulma.io/images/placeholders/1280x960.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div className="card-content">
-                <p className="title">PROJECT 6</p>
-                <p className="subtitle">Lorem Ipsum Dolor Sit Amet</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="column is-one-quarter">
-            <div className="card">
-              <div className="card-image">
-                <figure className="image is-4by3">
-                  <img
-                    src="https://bulma.io/images/placeholders/1280x960.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div className="card-content">
-                <p className="title">PROJECT 7</p>
-                <p className="subtitle">Lorem Ipsum Dolor Sit Amet</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="column is-one-quarter">
-            <div className="card">
-              <div className="card-image">
-                <figure className="image is-4by3">
-                  <img
-                    src="https://bulma.io/images/placeholders/1280x960.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div className="card-content">
-                <p className="title">PROJECT 8</p>
-                <p className="subtitle">Lorem Ipsum Dolor Sit Amet</p>
+              <div className="card-footer">
+                <a href="#" className="card-footer-item">
+                  <FaEye className="is-size-4" />
+                </a>
+                <a href="#" className="card-footer-item">
+                  <FaCode className="is-size-4" />
+                </a>
               </div>
             </div>
           </div>
