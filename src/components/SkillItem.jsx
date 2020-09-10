46df449e888d38270e8202a61cadb81435518809
Arunangshu Biswas <arunangshubsws@gmail.com>
2020-09-10 15:44:55 +0530

    feat: created skill item component

---
+++ src/components/SkillItem.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const SkillItem = ({ skill, rating }) => {
+  return (
+    <>
+      <p className="is-size-4">{skill}</p>
+      <progress className="progress is-primary" value={rating} max="100">
+        {`${rating}%`}
+      </progress>
+    </>
+  )
+}
+
+export default SkillItem
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,38 +1,37 @@
 import React from "react"
+import SkillItem from "./SkillItem"
 
 const Skills = () => {
   return (
     <section id="skills" className="pt-6 pb-6">
       <div className="container">
-        <h2 class="is-size-1 has-text-weight-bold has-text-centered mb-6">My Skillset</h2>
+        <h2 class="is-size-1 has-text-weight-bold has-text-centered mb-6">
+          My Skillset
+        </h2>
 
-        <div className="columns">
-          <div className="column">
-            <p className="is-size-4">C/C++</p>
-            <progress className="progress is-info" value="7" max="10">
-              70%
-            </progress>
+        <div className="columns is-multiline">
+          <div className="column is-one-quarter">
+            <SkillItem skill="C++" rating="75" />
           </div>
 
-          <div className="column">
-            <p className="is-size-4">Java</p>
-            <progress className="progress is-danger" value="8" max="10">
-              80%
-            </progress>
+          <div className="column is-one-quarter">
+            <SkillItem skill="Java" rating="65" />
           </div>
 
-          <div className="column">
-            <p className="is-size-4">Python</p>
-            <progress className="progress is-warning" value="7" max="10">
-              70%
-            </progress>
+          <div className="column is-one-quarter">
+            <SkillItem skill="Python" rating="65" />
           </div>
 
-          <div className="column">
-            <p className="is-size-4">Web Development</p>
-            <progress className="progress is-success" value="9" max="10">
-              90%
-            </progress>
+          <div className="column is-one-quarter">
+            <SkillItem skill="Backend Development" rating="75" />
+          </div>
+
+          <div className="column is-one-quarter">
+            <SkillItem skill="Frontend Development" rating="65" />
+          </div>
+
+          <div className="column is-one-quarter">
+            <SkillItem skill="Machine Learning" rating="60" />
           </div>
         </div>
       </div>
